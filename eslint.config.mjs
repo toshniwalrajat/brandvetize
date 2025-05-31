aad89d66ad11b0f4a5462edbb2364cfb47e71811
toshniwalrajat <rajattoshniwal2@gmail.com>
2025-05-31 15:49:04 +0530

    Fix ESLint unescaped entity error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    // You can apply this rule globally or to specific files using the 'files' property if needed
+    rules: {
+      "react/no-unescaped-entities": "off", // Disable the rule
+    },
+  },
+];
 
 export default eslintConfig;
